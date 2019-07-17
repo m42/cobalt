084d7e2212040ef87129a350856e171ec89e9d0c
Johan <johan.m42@gmail.com>
2019-07-17 17:56:08 +0200

    ConsoleUtils with TimedMessage

---
+++ Source/Cobalt.ConsoleTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Cobalt.ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cobalt.ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.TimedMessage("hello world", 3, true);
+
+        }
+    }
+}
---
+++ Source/Cobalt/ConsoleApp/ConsoleUtils.TimedMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Cobalt.ConsoleApp
+{
+    public partial class ConsoleUtils
+    {
+        /// <summary>
+        /// Show a message for a specified number of seconds before continuing.
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <param name="seconds">Seconds to show message</param>
+        /// <param name="showCountdown">Show countdown: "My message...3...2...1"</param>
+        public static void TimedMessage(string message, int seconds, bool showCountdown)
+        {
+            Console.Write(message);
+
+            for (int i = seconds; i > 0; i--)
+            {
+                if (showCountdown)
+                    Console.Write(string.Format("...{0}", i));
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
