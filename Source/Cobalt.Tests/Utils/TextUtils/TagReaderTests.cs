ad1c754199347d4412941220833141936a04e68f
Johan <johan.m42@gmail.com>
2019-07-17 22:00:34 +0200

    TextUtils / TagReader

---
+++ Source/Cobalt.Tests/Utils/TextUtils/TagReaderTests.cs
@@ -0,0 +1,63 @@
+﻿using Cobalt.Utils.TextUtils;
+using System.Text;
+using Xunit;
+
+namespace Cobalt.Tests.Utils.TextUtils
+{
+    public class TagReaderTests
+    {
+        private static string GetContent()
+        {
+            var content = new StringBuilder();
+            content.AppendLine("Just some ordinary text.");
+            content.AppendLine("<tagA>Value A</tagA>");
+            content.AppendLine("<tagB>Value B</tagB>");
+            content.AppendLine("<tagA>Another tagA</tagA>");
+            content.AppendLine("More ordinary text.");
+
+            return content.ToString();
+        }
+
+        [Fact]
+        public void GetTagValue_ReturnsCorrectValue_WhenCorrectInput()
+        {
+            var sutA = TagReader.GetTagValue(GetContent(), "tagA");
+            var sutB = TagReader.GetTagValue(GetContent(), "tagB");
+
+            Assert.Equal("Value A", sutA);
+            Assert.Equal("Value B", sutB);
+        }
+
+        [Fact]
+        public void GetTagValue_ReturnsEmpty_WhenContentIsNull()
+        {
+            var sut = TagReader.GetTagValue(null, "tagA");
+
+            Assert.Equal(string.Empty, sut);
+        }
+
+        [Fact]
+        public void GetTagValue_ReturnsEmpty_WhenContentIsEmpty()
+        {
+            var sut = TagReader.GetTagValue(string.Empty, "tagA");
+
+            Assert.Equal(string.Empty, sut);
+        }
+
+        [Fact]
+        public void GetTagValue_ReturnsEmpty_WhenTagIsNull()
+        {
+            var sut = TagReader.GetTagValue(GetContent(), null);
+
+            Assert.Equal(string.Empty, sut);
+        }
+
+        [Fact]
+        public void GetTagValue_ReturnsEmpty_WhenTagIsEmpty()
+        {
+            var sut = TagReader.GetTagValue(GetContent(), string.Empty);
+
+            Assert.Equal(string.Empty, sut);
+        }
+    }
+}
---
+++ Source/Cobalt/Utils/TextUtils/TagReader.cs
@@ -0,0 +1,33 @@
+﻿namespace Cobalt.Utils.TextUtils
+{
+    public partial class TagReader
+    {
+        /// <summary>
+        /// Get value between start/endtag in a string.
+        /// If multiple tags of same type, only value of the first is returned.
+        /// </summary>
+        /// <param name="content">The string</param>
+        /// <param name="tag">The tag to look for</param>
+        /// <returns>Value in tag</returns>
+        public static string GetTagValue(string content, string tag)
+        {
+            if (string.IsNullOrEmpty(content) || string.IsNullOrEmpty(tag))
+                return string.Empty;
+
+            var result = string.Empty;
+
+            if (content.ToLower().Contains(tag.ToLower()))
+            {
+                var startTag = $"<{tag}>";
+                var endTag = $"</{tag}>";
+                var startIndex = content.IndexOf(startTag) + startTag.Length;
+                var length = content.IndexOf(endTag) - startIndex;
+
+                result = content.Substring(startIndex, length);
+            }
+
+            return result;
+        }
+        
+    }
+}
